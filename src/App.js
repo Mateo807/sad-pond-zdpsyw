4008b2b8bf701f682681e648ed9b843d6de82471
Matthew Banda <bece21-mbanda@mubas.ac.mw>
2025-06-19 12:41:53 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+const list = ["name", "email", "location", "age"];
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>This is my Div</h1>
+      </div>
+      ;
+    </>
+  );
+}
